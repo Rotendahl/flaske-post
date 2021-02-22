ac8551696abb3a5f9211b3bdc321124b68b81a0c
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2021-02-22 22:20:03 +0100

    Week 2

--- src/app.py
+++ src/app.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Ohøj verden!'
+    return """
+        <h1>Hejsa styrmand</h1>
+        <h4>Her er en liste over flaskeposter</h4>
+        <ul>
+            <li>Jeg er sulten og fanget på havet</li>
+            <li>Jeg har spist min fugl</li>
+        </ul>
+    """
